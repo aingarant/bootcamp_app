207c9697cf5bbc8faa3fa187c0032a4fa9930b59
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-12-14 12:30:43 -0500

    Changed students to parameterized queries

--- students.js
+++ students.js
@@ -7,71 +7,21 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-// pool
-//   .query(
-//     `
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `
-//   )
-//   .then((res) => {
-//     console.log(res.rows);
-//   })
-//   .catch((err) => console.error("query error", err.stack));
-
-// pool
-//   .query(
-//     `
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `
-//   )
-//   .then((res) => {
-//     res.rows.forEach((user) => {
-//       console.log(
-//         `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
-//       );
-//     });
-//   });
-
-// pool
-//   .query(
-//     `
-// SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
-// FROM students
-// JOIN cohorts ON cohorts.id = cohort_id
-// LIMIT 5;
-// `
-//   )
-//   .then((res) => {
-//     res.rows.forEach((user) => {
-//       console.log(
-//         `${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`
-//       );
-//     });
-//   });
-
 const cohortName = process.argv.slice(2)[0];
 const limit = process.argv.slice(2)[1];
 
-
-pool
-  .query(
-    `
-SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
+const queryString = `SELECT students.id, students.name, cohort_id, cohorts.name AS cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE  '%${cohortName}%'
-LIMIT ${limit} ;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((user) => {
-      console.log(
-        `${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`
-      );
-    });
-  });
+WHERE cohorts.name LIKE  $1
+LIMIT $2 `;
 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((user) => {
+    console.log(
+      `${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`
+    );
+  });
+});
